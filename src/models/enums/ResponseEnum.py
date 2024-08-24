7d29ee298874e2b64f85d0aa9a0563c12cb57005
Lara Faroun <lara.r.faroun@gmail.com>
2024-08-24 23:26:15 +0300

    file uploading process.

---
+++ src/models/enums/ResponseEnum.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class ResponseSignal(Enum):
+    FILE_TYPE_NOT_SUPPORTED = "file_type_not_supported"
+    FILE_SIZE_EXCEEDED = "file_type_exceeded"
+    FILE_UPLOAD_SUCCESS = "file_upload_success"
+    FILE_UPLOAD_FAILED = "file_upload_failed"
+    File_VALIDATED_SUCCESS = "file_validated_sucessfully"
+    
