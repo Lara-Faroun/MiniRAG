1a4e9e1c5027089d2b440e2b55f6ae965c5a48dd
Lara Faroun <lara.r.faroun@gmail.com>
2024-10-01 10:17:03 +0300

    edit file loader method to handel none existing path

--- src/controllers/ProcessController.py
+++ src/controllers/ProcessController.py
@@ -20,6 +20,9 @@ class ProcessController(BaseController):
         file_path = os.path.join ( self.project_path , file_id)
         file_ext = self.get_file_extension(file_id=file_id)
 
+        if not os.path.exists(file_path):
+            return None
+
         if file_ext == ProcessingEnum.TXT.value:
             return TextLoader(file_path , encoding = "utf-8")
         
@@ -31,8 +34,11 @@ class ProcessController(BaseController):
     def get_file_content(self, file_id:str):
 
         loader = self.get_file_loader(file_id=file_id)
-        return loader.load()
-
+        if loader:
+            return loader.load()
+        else:
+            return None
+        
     def process_file_content(sele , file_content: list , file_id: str,
                              chunk_size: int=100 , overlap_size: int=20):
         
