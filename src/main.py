260c3ab8b303ba26d8865fb10da31a305fe69b84
Lara Faroun <lara.r.faroun@gmail.com>
2024-11-18 09:27:42 +0300

    fixis

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
-from routs import base, data
+from routs import base, data, nlp
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
 from stores.llm .LLMProviderFactory import LLMProviderFactory
 from stores.vectordb.VectorDBProviderFactory import VectorDBProviderFactory
+import logging
 
 app = FastAPI()
 
+@app.on_event("startup")
 async def startup_span():
     settings = get_settings()
 
@@ -24,15 +26,22 @@ async def startup_span():
     app.embedding_client = llm_provider_factory.create(provider=settings.EMBEDDING_BACKEND)
     app.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODEL_ID, embedding_size=settings.EMBEDDING_MODEL_SIZE)
     
+    #vectordb client
     app.vectordb_client = vectordb_provider_factory.create(provider= settings.VECTOR_DB_BACKEND)
     app.vectordb_client.connect()
 
+@app.on_event("shutdown")
 async def shutdown_span():
     app.mongo_conn.close()
     app.vectordb_client.disconnect()
 
-app.router.lifespan.on_startup.append(startup_span)
-app.router.lifespan.on_shutdown.append(shutdown_span)
+# app.router.lifespan.on_startup.append(startup_span)
+# app.router.lifespan.on_shutdown.append(shutdown_span)
+
+# app.on_event("startup")(startup_span)
+# app.on_event("shutdown")(shutdown_span)
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
+app.include_router(nlp.nlp_router)
+
