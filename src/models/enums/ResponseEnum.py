e87c09bec7f2df160f324ce62659ef14f1bd7f0b
Lara Faroun <lara.r.faroun@gmail.com>
2024-11-18 09:30:33 +0300

    adding new response messages

--- src/models/enums/ResponseEnum.py
+++ src/models/enums/ResponseEnum.py
@@ -10,4 +10,11 @@ class ResponseSignal(Enum):
     PROCESSING_SUCCESS = "processing_success"
     NO_FILES_ERROR = "not found files"
     FILE_ID_ERROR = "no file with this id"
+    PROJECCT_NOT_FOUND_ERROR = "project_not_found"
+    INSERT_INTO_VECTORDB_ERROR = "insert_into_vectordb_error"
+    INSERT_INTO_VECTORDB_SUCCESS = "insert_into_vectordb_success"
+    VECTORDB_COLLECTION_RETRIEVED = "vectordb_collection_retrieved"
+    VECTORDB_SEARCH_ERROR = "vectordb_search_error"
+    VECTORDB_SEARCH_SUCCESS= "vectordb_search_success"
+    
     
